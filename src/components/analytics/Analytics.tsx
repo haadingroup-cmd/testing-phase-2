803f362521ec311c66aa14830f3e2d0f8c03a095
haadingroup-cmd <haadingroup@gmail.com>
2026-08-10 21:31:52 +0500

    Update Analytics.tsx

--- src/components/analytics/Analytics.tsx
+++ src/components/analytics/Analytics.tsx
@@ -1,24 +1,39 @@
 "use client";
-
 import Script from "next/script";
 
-const GA_ID = "G-XMKM25RD0F";
+// IDs come from Vercel env vars (NEXT_PUBLIC_*), set under
+// Settings -> Environment Variables. If a var isn't set yet, that
+// script simply doesn't load -- no code changes needed later.
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
 export default function Analytics() {
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="ga4-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_ID}');
-        `}
-      </Script>
+      {GA_ID ? (
+        <>
+          <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+          <Script id="ga4-init" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');`}
+          </Script>
+        </>
+      ) : null}
+
+      {PIXEL_ID ? (
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;
+            s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${PIXEL_ID}');
+            fbq('track', 'PageView');`}
+        </Script>
+      ) : null}
     </>
   );
 }
